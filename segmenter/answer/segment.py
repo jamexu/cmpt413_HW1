0ca4ea39b23f2af195967fa6d84fc5f97f089644
jamie x <jamie x>
2016-09-23 14:00:22 -0700

    jamie's edit to combine scripts.  Included utf-heading for ability to print unicode.  Edited opt parser and created own unigram word dictionary

--- segmenter/answer/segment.py
+++ segmenter/answer/segment.py
@@ -1,34 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import sys, codecs, optparse, os, heapq, math
 
-import sys, codecs, optparse, os
+def opt_parser():
+    optparser = optparse.OptionParser()
+    optparser.add_option("-c", "--unigramcounts", dest='counts1w', default=os.path.join('../data', 'count_1w.txt'), help="unigram counts")
+    optparser.add_option("-b", "--bigramcounts", dest='counts2w', default=os.path.join('../data', 'count_2w.txt'), help="bigram counts")
+    optparser.add_option("-i", "--inputfile", dest="input", default=os.path.join('../data', 'input'), help="input file to segment")
+    (opts, _) = optparser.parse_args()
+    return opts
 
-optparser = optparse.OptionParser()
-optparser.add_option("-c", "--unigramcounts", dest='counts1w', default=os.path.join('data', 'count_1w.txt'), help="unigram counts")
-optparser.add_option("-b", "--bigramcounts", dest='counts2w', default=os.path.join('data', 'count_2w.txt'), help="bigram counts")
-optparser.add_option("-i", "--inputfile", dest="input", default=os.path.join('data', 'input'), help="input file to segment")
-(opts, _) = optparser.parse_args()
+# Imports counts1w file into a Python dictionary using the word as key and count as value
+# Also sums the total number of word counts in order to generate probability
+class unigram_pdist(object):
+    # Create a python dictionary to store unigram counts and keep track of total word counts
+    def __init__(self, unigram_counts):
+        self.counts1w_dist = {}
+        self.word_sum = 0
+        self.import_unigram(unigram_counts)
 
-class Pdist(dict):
-    "A probability distribution estimated from counts in datafile."
-
-    def __init__(self, filename, sep='\t', N=None, missingfn=None):
-        self.maxlen = 0 
-        for line in file(filename):
-            (key, freq) = line.split(sep)
-            try:
-                utf8key = unicode(key, 'utf-8')
-            except:
-                raise ValueError("Unexpected error %s" % (sys.exc_info()[0]))
-            self[utf8key] = self.get(utf8key, 0) + int(freq)
-            self.maxlen = max(len(utf8key), self.maxlen)
-        self.N = float(N or sum(self.itervalues()))
-        self.missingfn = missingfn or (lambda k, N: 1./N)
-
-    def __call__(self, key):
-        if key in self: return float(self[key])/float(self.N)
-        #else: return self.missingfn(key, self.N)
-        elif len(key) == 1: return self.missingfn(key, self.N)
-        else: return None
-
+    # Create a python dictionary to store unigram counts and keep track of total word counts
+    def import_unigram(self, input_file):
+        for line in open(input_file):
+            key = unicode(line.split("\t")[0], 'utf-8')
+            value = float(line.split("\t")[1])
+            self.counts1w_dist[key] = value
+            self.word_sum += value
 
 
 
@@ -39,7 +36,6 @@ class Pdist(dict):
 # It will fill a L-long 1-dimensional array (where L is the length of line)
 # with Memo objects which contain a P value and a pointer to a predecessor
 # such that the ith entry will be the best possible P value for a the string from 0 to i
-
 class Memo():
 
     def __init__(self, value, pred, word):
@@ -90,21 +86,14 @@ def memo_segmenter(line):
 def arg_max(word, pred):
     return 1
 
+# MAIN FUNCTION STARTS HERE
+if __name__ == "__main__":
+    # get commandline arguments
+    args = opt_parser()
+    # Execute depending on which segmenter model
+    if args.counts1w:
+        uni_pdist = unigram_pdist(path)
+    elif args.counts2w:
+        pass
 
 
-
-
-
-
-# the default segmenter does not use any probabilities, but you could ...
-Pw  = Pdist(opts.counts1w)
-
-old = sys.stdout
-sys.stdout = codecs.lookup('utf-8')[-1](sys.stdout)
-# ignoring the dictionary provided in opts.counts
-with open(opts.input) as f:
-    for line in f:
-        utf8line = unicode(line.strip(), 'utf-8')
-        output = [i for i in utf8line]  # segmentation is one word per character in the input
-        print " ".join(output)
-sys.stdout = old
