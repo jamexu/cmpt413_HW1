d0e7135eaadc8a83b532394ef6c42a32ee2e1814
YOUNGSHIN OH <youngshin.dev@gmail.com>
2016-09-27 11:50:36 -0700

    trying sum less than 1 for weights

--- segmenter/default.py
+++ segmenter/default.py
@@ -128,7 +128,7 @@ def bigram_arg_max(word,pred):
 def Bigram_Jelinek(word,pred):
 
     unigram_count=float(get_uni_count(word))
-    unigram_prob= float(0.9999999999)*unigram_count/float(N) +float(0.0000000001)*(float(0.01)/float(N))
+    unigram_prob= float(0.9999999999)*unigram_count/float(N) +float(0.0000000001)*(float(0.1)/float(N))
     if pred==None:
         return math.log(unigram_prob,2)
 
@@ -136,9 +136,9 @@ def Bigram_Jelinek(word,pred):
     unigram_log_prob= math.log(unigram_prob,2)
     if bigram_count!=0:
         bigram_prob=math.log(float(bigram_count)/float(N2),2)-pred.value
-        return numpy.logaddexp2(math.log(0.3,2)+bigram_prob,math.log(0.7,2)+unigram_log_prob)
+        return numpy.logaddexp2(math.log(0.8,2)+bigram_prob,math.log(0.1,2)+unigram_log_prob)
     else:
-        return math.log(float(0.7)*unigram_prob,2)
+        return math.log(float(0.1)*unigram_prob,2)
 
 
 def get_bi_count(word,pred):
